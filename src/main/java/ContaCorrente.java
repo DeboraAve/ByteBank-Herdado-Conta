9a35806910912ca06d90836c67672c8762a51f4a
DeboraAve <debora.avelino98@gmail.com>
2023-07-13 11:48:29 -0300

    Treinando exceções

--- src/main/java/ContaCorrente.java
+++ src/main/java/ContaCorrente.java
@@ -3,9 +3,9 @@ public class ContaCorrente extends Conta implements Tributavel {
          super(agencia, numero);
      }
     @Override
-    public boolean saca(double valor) {
+    public void saca(double valor) throws SaldoInsuficienteException {
          double valorASacar = valor + 0.2;
-        return super.saca(valorASacar);
+         super.saca(valorASacar);
     }
 
     @Override
